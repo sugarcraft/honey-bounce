0a5ed7f8c4392e409a03872df40b6acf9e6e780e
Joe Huss <detain@interserver.net>
2026-05-07 05:50:43 -0400

    Add Japanese locale for all libs (#254)

---
+++ lang/ja.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * Japanese translations for honey-bounce.
+ *
+ * @return array<string, string>
+ */
+
+declare(strict_types=1);
+
+return [
+    'spring.fps_positive' => 'fps は > 0 である必要があります；取得：{fps}',
+];
