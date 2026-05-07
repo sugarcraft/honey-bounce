65fa47eb62b691b4a2c075417bd677f22eae21c7
Joe Huss <detain@interserver.net>
2026-05-07 05:45:19 -0400

    Add remaining Portuguese locale files for all libs (#251)

---
+++ lang/pt.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * Portuguese translations for honey-bounce.
+ *
+ * @return array<string, string>
+ */
+
+declare(strict_types=1);
+
+return [
+    'spring.fps_positive' => 'fps tem de ser > 0; obtido: {fps}',
+];
