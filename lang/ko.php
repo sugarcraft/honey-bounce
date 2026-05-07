a31eb205ce4cdf1bed3a9a4cfcbc0ab8c390ed3c
Joe Huss <detain@interserver.net>
2026-05-07 05:56:41 -0400

    Add Korean locale for all libs (#257)

---
+++ lang/ko.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * Korean translations for honey-bounce.
+ *
+ * @return array<string, string>
+ */
+
+declare(strict_types=1);
+
+return [
+    'spring.fps_positive' => 'fps는 > 0이어야 합니다; 받음: {fps}',
+];
